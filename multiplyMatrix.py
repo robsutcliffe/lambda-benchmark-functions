0855e98d70b87a98eb51a0fd875259f9ae1b30c4
robsutcliffe <rob@firefields.com>
2023-12-21 19:34:51 +0100

    check we're getting the data correctly.

--- multiplyMatrix.py
+++ multiplyMatrix.py
@@ -10,8 +10,8 @@ def handler(event, context):
     matrix_a = event.get('matrix_a')
     matrix_b = event.get('matrix_b')
 
-    if not matrix_a or not matrix_b or len(matrix_a) != len(matrix_a[0]) or len(matrix_b) != len(matrix_b[0]):
-        return {"statusCode": 400, "body": "Invalid input matrices"}
+    # if not matrix_a or not matrix_b or len(matrix_a) != len(matrix_a[0]) or len(matrix_b) != len(matrix_b[0]):
+    #     return {"statusCode": 400, "body": "Invalid input matrices"}
 
-    result = matrix_multiply(matrix_a, matrix_b)
-    return {"statusCode": 200, "body": json.dumps(result)}
+    # result = matrix_multiply(matrix_a, matrix_b)
+    return {"statusCode": 200, "body": json.dumps(event)}
