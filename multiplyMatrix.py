23b1e4b2088142ec069d59d3d449c17508eada07
robsutcliffe <rob@firefields.com>
2023-12-21 19:39:13 +0100

    check formating of matrix

--- multiplyMatrix.py
+++ multiplyMatrix.py
@@ -12,6 +12,6 @@ def handler(event, context):
 
     # if not matrix_a or not matrix_b or len(matrix_a) != len(matrix_a[0]) or len(matrix_b) != len(matrix_b[0]):
     #     return {"statusCode": 400, "body": "Invalid input matrices"}
-
+    #
     # result = matrix_multiply(matrix_a, matrix_b)
-    return {"statusCode": 200, "body": json.dumps(event)}
+    return {"statusCode": 200, "body": json.dumps({ matrix_a, matrix_b })}
