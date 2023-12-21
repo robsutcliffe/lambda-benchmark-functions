61d7a08509e6f21b7d6ebfdb71cd9bdf4766c77f
robsutcliffe <rob@firefields.com>
2023-12-21 20:05:40 +0100

    retry whole function

--- multiplyMatrix.py
+++ multiplyMatrix.py
@@ -9,17 +9,17 @@ def matrix_multiply(matrix_a, matrix_b):
 def handler(event, context):
     body = json.loads(event['body'])
 
-    # matrix_a = body.get('matrix_a')
-    # matrix_b = body.get('matrix_b')
-    #
-    # if not matrix_a or not matrix_b:
-    #     return {"statusCode": 400, "body": "matrices not received"}
-    #
-    # if len(matrix_a) != len(matrix_a[0]):
-    #     return {"statusCode": 400, "body": "matrices should be same length"}
-    #
-    # if len(matrix_b) != len(matrix_b[0]):
-    #     return {"statusCode": 400, "body": "matrices are not same length and breadth"}
-    #
-    # result = matrix_multiply(matrix_a, matrix_b)
-    return {"statusCode": 200, "body": json.dumps(body)}
+    matrix_a = body.get('matrix_a')
+    matrix_b = body.get('matrix_b')
+
+    if not matrix_a or not matrix_b:
+        return {"statusCode": 400, "body": "matrices not received"}
+
+    if len(matrix_a) != len(matrix_a[0]):
+        return {"statusCode": 400, "body": "matrices should be same length"}
+
+    if len(matrix_b) != len(matrix_b[0]):
+        return {"statusCode": 400, "body": "matrices are not same length and breadth"}
+
+    result = matrix_multiply(matrix_a, matrix_b)
+    return {"statusCode": 200, "body": json.dumps(result)}
