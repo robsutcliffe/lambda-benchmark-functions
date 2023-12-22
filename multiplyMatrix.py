7077f1bf1365dbc4013f313d0023518e7760fc03
robsutcliffe <rob@firefields.com>
2023-12-22 17:59:57 +0100

    get computation time

--- multiplyMatrix.py
+++ multiplyMatrix.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 import json
+import time
 
 def generate_matrices(matrix_size):
     matrices = [[[random.random() for _ in range(matrix_size)] for _ in range(matrix_size)],
@@ -15,12 +16,14 @@ def handler(event, context):
     matrix_size_str = event['queryStringParameters']['matrix_size']
     matrix_size = int(matrix_size_str)
 
+    start = time.time()
     if not matrix_size:
         return {"statusCode": 400, "body": "matrix_size not received"}
 
     matrix_a, matrix_b = generate_matrices(matrix_size)
 
-    result = matrix_multiply(matrix_a, matrix_b)
+    matrix_multiply(matrix_a, matrix_b)
+    end = time.time()
 
     cors_headers = {
         "Access-Control-Allow-Origin": "https://robsutcliffe.static.observableusercontent.com",
@@ -29,7 +32,7 @@ def handler(event, context):
 
     response = {
         "statusCode": 200,
-        "body": json.dumps({ "success": "true" }),
+        "body": json.dumps({ "run-time": end-start }),
         "headers": cors_headers
     }
 
